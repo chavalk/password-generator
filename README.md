a8a6d2141b29d5ca9573fa4305e25964756a04ec
chavalk <chavalk@hotmail.com>
2020-10-31 20:54:56 -0500

    Adding most recent code with comments and readme.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # password-generator
+
+## Password-Generator Project
+
+This project involved creating the logic in javascript for the application to generate a random password using uppercase, lowercase, numbers, and/or symbols. The use of the four options mentioned depends on the user selection. The user indicates the length of the password with a minimum requirement of 8 characters and a maximum of 128 characters. 
+
+The very first prompt the user is presented with after clicking the generate password button is for the length of the password. If the user enters a number outside of the mentioned parameter, a while loop will alert the user that invalid information was entered in and the prompt will be presented again.
+
+Once the user enters a valid number for the legth of the password, the first confirm is presented to the user. The user is asked to indicate the use of uppercase, lowercase, numbers, and/or symbols for the password to be generated. The user can select any combination of these character types. If the user does not make a selection, a while loop will alert the user to select at least one character type.
+
+After the user makes at least one selection, the application will generate a random password with the specified length
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variables for password content
 var alphabetLowercase = "abcdefghijklmnopqrstuvwxyz";
 var alphabetUppercase  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
@@ -16,17 +17,29 @@ function writePassword() {
 
 }
 
+// Generate password using selections made by user with prompts and confirms. Return password to write
+// password function.
 function generatePassword() {
+
+  // Prompt user for password length.
   var passwordLength = prompt("Please select a password length between 8 and 128 characters.");
+
+  // Variable declared to store password that's generated.
   var password = "";
+
+  // While loop to make sure user selects correct password length.
   while(passwordLength < 8 || passwordLength > 128){
     alert("Invalid password length.");
     passwordLength = prompt("Please select a password length between 8 and 128 characters.");
   }
+
+  // Confirm variables declared to store user selection on character types.
   var uppercaseConfirm = confirm("Would you like to include uppercase letters?");
   var lowercaseConfirm = confirm("Would you like to include lowercase letters?");
   var numbersConfirm = confirm("Would you like to include numbers?");
   var symbolsConfirm = confirm("Would you like to include symbols?");
+
+  // While loop to make sure user selects at least one character type.
   while(!uppercaseConfirm && !lowercaseConfirm && !numbersConfirm && !symbolsConfirm){
     alert("Please select at least one character type.");
     uppercaseConfirm = confirm("Would you like to include uppercase letters?");
@@ -34,30 +47,64 @@ function generatePassword() {
     numbersConfirm = confirm("Would you like to include numbers?");
     symbolsConfirm = confirm("Would you like to include symbols?");
   }
+
+  // If statement to generate random password when user selects all four character types.
   if(uppercaseConfirm && lowercaseConfirm && numbersConfirm && symbolsConfirm){
+    
+    // For loop to generate password with all four character types.
     for (var i = 0; i < passwordLength; i++) {
-      alphabetLowercase += alphabetUppercase + numbers + symbols;
+      // alphabetLowercase += alphabetUppercase + numbers + symbols;
       password += alphabetLowercase[Math.floor(Math.random() * alphabetLowercase.length)];
-    }
-    return password; 
-  } else if(lowercaseConfirm && numbersConfirm && symbolsConfirm){
-    for (var i = 0; i < passwordLength; i++) {
-      alphabetLowercase += numbers + symbols;
-      password += alphabetLowercase[Math.floor(Math.random() * alphabetLowercase.length)];
-    }
-      return password; 
-  } else if(numbersConfirm && symbolsConfirm){
-    for (var i = 0; i < passwordLength; i++) {
-      numbers += symbols;
+      password += alphabetUppercase[Math.floor(Math.random() * alphabetUppercase.length)];
       password += numbers[Math.floor(Math.random() * numbers.length)];
+      password += symbols[Math.floor(Math.random() * symbols.length)];
     }
+
     return password; 
-  } else {
+
+  } 
+  
+  // Else if statement to generate random password when user selects lowercase, numbers, and symbols.
+  else if(lowercaseConfirm && numbersConfirm && symbolsConfirm){
+    
+    // For loop to generate random password when user selects lowercase, numbers, and symbols.
+    for (var i = 0; i < passwordLength; i++) {
+      // alphabetLowercase += numbers + symbols;
+      password += alphabetLowercase[Math.floor(Math.random() * alphabetLowercase.length)];
+      password += numbers[Math.floor(Math.random() * numbers.length)];
+      password += symbols[Math.floor(Math.random() * symbols.length)];
+    }
+
+    return password; 
+
+  } 
+  
+  // Else if statement to generate random password when user selects numbers and symbols.
+  else if(numbersConfirm && symbolsConfirm){
+    
+    // For loop to generate random password when user selects numbers and symbols.
+    for (var i = 0; i < passwordLength; i++) {
+      // numbers += symbols;
+      password += numbers[Math.floor(Math.random() * numbers.length)];
+      password += symbols[Math.floor(Math.random() * symbols.length)];
+    }
+
+    return password; 
+
+  } 
+  
+  // Else statement when user only selects symbols.
+  else {
+
+    // For loop to generate random password when user selects symbols.
     for (var i = 0; i < passwordLength; i++) {
       password += symbols[Math.floor(Math.random() * symbols.length)];
     }
+
     return password; 
+
   } 
+
 }
 
 // Add event listener to generate button
